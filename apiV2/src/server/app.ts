12fb5b0017d6c00eb233347f2e952a1fa4f35682
Alex Tomas <atomasllimos@gmail.com>
2023-01-01 10:23:10 +0100

    style: Delete some unnecessary lines

--- apiV2/src/server/app.ts
+++ apiV2/src/server/app.ts
@@ -1,25 +1,15 @@
 import cors from 'cors'
 import express, { Application } from 'express'
 import morgan from 'morgan'
-// import { initializeData } from '../../../dbSeedingV2/index.js'
 import '../db/connection.js'
-// import Restaurant from '../models/Restaurant.js'
-// import User from '../models/User.js'
-import { initializeData } from '../../../dbSeedingV2/index.js'
-import Restaurant from '../models/Restaurant.js'
-import User from '../models/User.js'
 import authRoutes from '../routes/auth.routes.js'
 import restaurantsRoutes from '../routes/restaurants.routes.js'
 import usersRouter from '../routes/users.routes.js'
 import cloudinary from '../services/cloudinary.js'
 const app: Application = express()
 
-await initializeData(Restaurant, User)
-
 cloudinary.setUp()
-// await initializeData(Restaurant, User)
-app.set('port', process.env.PORT ?? 3001)
-await initializeData(Restaurant, User)
+app.set('port', process.env.PORT || 3001)
 // Middlewares
 app.use(cors())
 app.use(express.json())
